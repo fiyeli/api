32c4f79c8ae286a6688dcf2aa344f6869028cc51
Cyril Dussert <cyril.dussert@orange.com>
2018-11-26 09:08:33 +0100

    Ajout handler 404

--- api.py
+++ api.py
@@ -1,7 +1,18 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort
+
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def hello_world():
     return jsonify({'message': 'Hello World !'})
+
+
+@app.route('/stats')
+def all_stats():
+    abort(404)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return jsonify({'message': 'What the fuck you are doing here ?'}), 404
