6e56c3c23bbdd28c4720714102b8edeff406fc43
Cyril Dussert <cyril.dussert@orange.com>
2018-11-26 09:27:48 +0100

    Ajout 2 endpoints
    Ajout 2 endpoints

--- api.py
+++ api.py
@@ -8,11 +8,21 @@ def hello_world():
     return jsonify({'message': 'Hello World !'})
 
 
-@app.route('/stats')
-def all_stats():
+@app.route('/stats', methods=['GET'])
+def stats():
+    abort(404)
+
+
+@app.route('/stats/today', methods=['GET'])
+def stats_today():
+    abort(404)
+
+
+@app.route('/stats/<date>', methods=['GET'])
+def stats_day(date):
     abort(404)
 
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return jsonify({'message': 'What the fuck you are doing here ?'}), 404
+    return jsonify({'message': str(error)}), 404
