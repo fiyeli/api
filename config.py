107dfa337e1f388baefdad2045f84751de8c7e32
Cyril Dussert <cyril.dussert@orange.com>
2019-01-17 10:51:58 +0100

    Adding Gunicorn config file

---
+++ config.py
@@ -0,0 +1,22 @@
+# Sample Gunicorn configuration file.
+
+#
+# Server socket
+#
+#   bind - The socket to bind.
+#
+#       A string of the form: 'HOST', 'HOST:PORT', 'unix:PATH'.
+#       An IP is a valid HOST.
+#
+#   backlog - The number of pending connections. This refers
+#       to the number of clients that can be waiting to be
+#       served. Exceeding this number results in the client
+#       getting an error when attempting to connect. It should
+#       only affect servers under significant load.
+#
+#       Must be a positive integer. Generally set in the 64-2048
+#       range.
+#
+
+bind = '0.0.0.0:8000'
+backlog = 2048
